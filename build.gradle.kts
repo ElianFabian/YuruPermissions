b7fc4fcab91378699a01b289472696cd8753147e
Elián Fabián <elian.fabian.2001@gmail.com>
2026-06-26 18:57:29 +0200

    Prepare to publish library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,42 @@
+import com.android.build.api.dsl.LibraryExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
 	alias(libs.plugins.android.application) apply false
 	alias(libs.plugins.kotlin.compose) apply false
 	alias(libs.plugins.android.library) apply false
+	id("maven-publish")
+}
+
+subprojects {
+	val libraryModules = listOf("yuru-permissions")
+	if (name in libraryModules) {
+		val libs = rootProject.extensions.getByType<VersionCatalogsExtension>().named("libs")
+		group = libs.findVersion("yuruProviderGroup").get().requiredVersion
+		version = libs.findVersion("yuruProviderVersion").get().requiredVersion
+
+		plugins.withId("com.android.library") {
+			plugins.apply("maven-publish")
+			configure<LibraryExtension> {
+				publishing {
+					singleVariant("release") {
+						withSourcesJar()
+					}
+				}
+			}
+			afterEvaluate {
+				extensions.configure<PublishingExtension> {
+					publications {
+						create<MavenPublication>("release") {
+							from(components.findByName("release") ?: components["release"])
+
+							groupId = project.group.toString()
+							artifactId = project.name
+							version = project.version.toString()
+						}
+					}
+				}
+			}
+		}
+	}
 }
